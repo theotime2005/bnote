f08c54ed537fd3084bcbd81a97e8a4cc35ca70d8
theotime2005 <theotime.berthod@epitech.digital>
2024-11-17 10:26:46 +0100

    fix: Correct compare date

--- bnote/apps/agenda/agenda.py
+++ bnote/apps/agenda/agenda.py
@@ -1,7 +1,7 @@
 """
  bnote project
- Author : Eurobraille
- Date : 2024-07-16
+ Author : Theotime Berthod
+ Date : 2022-11-28
  Licence : Ce fichier est libre de droit. Vous pouvez le modifier et le redistribuer à votre guise.
 """
 
