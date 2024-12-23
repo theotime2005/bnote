98685194928a1ea17d8b884c1bd10eb3df6af28f
theotime2005 <theotime.berthod@epitech.digital>
2024-12-23 12:19:40 +0100

    Revert "feat: Option to compile only .mo filses"
    This reverts commit 4be47054449ff63eb8b06d855bc480d5c1bc36ed.

--- bnote/translation/update.sh
+++ bnote/translation/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Ce script met à jour toutes les traductions à partir du fichier .pot lui-même mis à jour.
+# Ce script met à jour toutes les traduction à partir du fichier .pot lui-même mis à jour.
 # Ce script se lance depuis un terminal pointant le dossier dans lequel il se trouve (sh ./update.sh)
 
 # Chemin vers le dossier contenant vos fichiers .po et .pot
@@ -7,26 +7,15 @@ PO_FILES_PATH="."
 # Chemin vers le fichier .pot
 POT_FILE="bnote.pot"
 
-# Si appelé avec l'argument "mo", ne faire que la compilation des fichiers .mo
-if [ "$1" == "mo" ]; then
-    echo "Compilation des fichiers .mo uniquement."
-    for po_file in $PO_FILES_PATH/*.po; do
-        echo "Compilation de $po_file vers ${po_file%.po}.mo"
-        msgfmt "$po_file" -o "${po_file%.po}.mo"
-    done
-    echo "Compilation terminée."
-    exit 0
-fi
-
 # Mise à jour du fichier .pot
-find .. -name "*.py" | xargs xgettext -o "$POT_FILE" --language=Python --from-code=UTF-8
-
+find .. -name *.py | xargs xgettext -o "$POT_FILE" --language=Python --from-code=UTF-8
+#xgettext -o "$POT_FILE" fichier1.c fichier2.c
 # Boucle sur chaque fichier .po
 for po_file in $PO_FILES_PATH/*.po; do
-    echo "Mise à jour du fichier $po_file"
-    msgmerge -U "$po_file" "$POT_FILE"
-    # Génère le fichier .mo correspondant
-    msgfmt "$po_file" -o "${po_file%.po}.mo"
+echo "Mise à jour du fichier $po_file"
+msgmerge -U "$po_file" "$POT_FILE"
+# Génère le fichier .mo correspondant
+msgfmt "$po_file" -o "${po_file%.po}.mo"
 done
 
 echo "Mise à jour terminée."
