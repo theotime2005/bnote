de76fda790824b8d296a234345c1c20d5ec162d2
theotime2005 <theotime.berthod@epitech.digital>
2025-01-10 19:28:32 +0100

    Revert "fix: Change API to set date"
    This reverts commit 3206a752754dd8e3c39aad5477fad17572e12cb9.

--- bnote/tools/sync_date.py
+++ bnote/tools/sync_date.py
@@ -4,42 +4,49 @@
  Date : 2024-07-16
  Licence : Ce fichier est libre de droit. Vous pouvez le modifier et le redistribuer à votre guise.
 """
+import datetime
 import requests
 import threading
 
-API_URL="https://timeapi.io/api"
 class SyncDate(threading.Thread):
-    def __init__(self, timezone, function):
+    def __init__(self, function):
         threading.Thread.__init__(self)
-        self.timezone = timezone
+        self.date=dict()
         self.function = function
 
     def run(self):
         current_date = self.get_current_date_time()
-        print(current_date)
-        if current_date:
-            self.function(year=int(current_date['year']), month=int(current_date['month']), day=int(current_date['day']), hour=int(current_date['hour']), minute=int(current_date['minute']), second=int(current_date['seconds']))
+        self.date = self.transforme_date_time(current_date)
+        if self.date:
+            self.function(year=int(self.date['year']), month=int(self.date['month']), day=int(self.date['day']), hour=int(self.date['hour']), minute=int(self.date['minute']), second=int(self.date['second']))
 
     def get_current_date_time(self):
         try:
-            response = requests.get(f'{API_URL}/time/current/zone', params={'timeZone': self.timezone})
+            response = requests.get('http://worldtimeapi.org/api/ip')
             data = response.json()
-            return data
+    
+            current_datetime = data['datetime']
+            return current_datetime
         except Exception as e:
             print(f"Error: {e}")
             return None
 
-def get_timezone_list(actual):
-    """
-    Return list of timezones and index of actual timezone
-    :param actual: str
-    :return: (list, str) | None
-    """
-    try:
-        request = requests.get(f"{API_URL}/timezone/availabletimezones")
-        response = request.json()
-        index = response.index(actual)
-        return (response, index)
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+    def transforme_date_time(self, date_str):
+        if not date_str:
+            return False
+    
+        # Utilisation du bon format pour strptime
+        try:
+            convert_date = datetime.datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f%z")
+        except Exception as e:
+            print(f"Conversion Error: {e}")
+            return False
+    
+        return {
+            "year": convert_date.year,
+            "month": convert_date.month,
+            "day": convert_date.day,
+            "hour": convert_date.hour,
+            "minute": convert_date.minute,
+            "second": convert_date.second
+        }
